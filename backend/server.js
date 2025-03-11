9270575901fa8cb48c24374bf466c484daad15ff
Shashiran1124 <viper.jsp@gmail.com>
2025-03-11 23:07:50 +0530

    //initial commit

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors()); // Enable Cross-Origin Requests
+app.use(express.json()); // For parsing application/json
+
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("MongoDB Connection Success!");
+    });
+
+
+
+// Example route
+app.get("/", (req, res) => {
+    res.send("Hello from Backend!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/")
+      .then(response => setMessage(response.data))
+      .catch(error => console.log(error));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Backend Message: {message}</h1>
+    </div>
+  );
+}
+
+export default App;
